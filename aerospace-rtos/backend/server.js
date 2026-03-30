e9e1a32238eada9e774a56b5b6b9c9cea58b1ae6
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-03-30 21:42:22 +0300

    Add files via upload

--- aerospace-rtos/backend/server.js
+++ aerospace-rtos/backend/server.js
@@ -1,1 +1,28 @@
 
+const WebSocket = require('ws');
+const { Scheduler } = require('./rtos/scheduler');
+const { generateTelemetry } = require('./telemetry/stream');
+const { autopilot } = require('./autopilot/modes');
+
+const wss = new WebSocket.Server({ port: 4000 });
+
+const scheduler = new Scheduler();
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  let state = {
+    altitude: 1000,
+    velocity: 200,
+    pitch: 0,
+    roll: 0,
+    yaw: 0
+  };
+
+  setInterval(() => {
+    scheduler.run();
+    state = autopilot(state);
+    const data = generateTelemetry(state);
+    ws.send(JSON.stringify(data));
+  }, 100);
+});
