69cc43a4f3429c89ff6be4b78e3f26c2b5175af4
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-03-30 21:43:08 +0300

    Add files via upload

--- aerospace-rtos/backend/autopilot/modes.js
+++ aerospace-rtos/backend/autopilot/modes.js
@@ -1,1 +1,17 @@
 
+function autopilot(state){
+  // simple altitude hold + stabilization
+  if(state.altitude < 1000){
+    state.altitude += 5;
+  } else if(state.altitude > 1000){
+    state.altitude -= 5;
+  }
+
+  state.pitch = (Math.random()*2-1).toFixed(2);
+  state.roll = (Math.random()*2-1).toFixed(2);
+  state.yaw = (state.yaw + 2) % 360;
+
+  return state;
+}
+
+module.exports = { autopilot };
