8dd596123df9a8b81467323696e6040d31e1ea8c
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-03-30 21:44:33 +0300

    Add files via upload

--- aerospace-rtos/backend/rtos/scheduler.js
+++ aerospace-rtos/backend/rtos/scheduler.js
@@ -1,1 +1,35 @@
 
+class Task {
+  constructor(name, priority, execute) {
+    this.name = name;
+    this.priority = priority;
+    this.execute = execute;
+  }
+}
+
+class Scheduler {
+  constructor() {
+    this.queue = [];
+    this.init();
+  }
+
+  init() {
+    this.addTask(new Task("FlightControl", 5, () => {}));
+    this.addTask(new Task("Navigation", 3, () => {}));
+    this.addTask(new Task("Telemetry", 1, () => {}));
+  }
+
+  addTask(task) {
+    this.queue.push(task);
+    this.queue.sort((a,b)=>b.priority-a.priority);
+  }
+
+  run() {
+    const t = this.queue.shift();
+    if (!t) return;
+    t.execute();
+    this.addTask(t);
+  }
+}
+
+module.exports = { Scheduler };
