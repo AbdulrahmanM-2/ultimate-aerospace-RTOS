4be45a23fe8c36c353edbedf332530475f5067c1
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-03-31 05:12:01 +0300

    Add files via upload

--- aerospace-rtos/control/tmr_controller.py
+++ aerospace-rtos/control/tmr_controller.py
@@ -1,1 +1,9 @@
 
+def vote(a,b,c):
+    if abs(a-b)<0.1: return (a+b)/2
+    if abs(a-c)<0.1: return (a+c)/2
+    if abs(b-c)<0.1: return (b+c)/2
+    return None
+
+def failsafe_mode():
+    return {"roll":0,"pitch":0,"throttle":0.5}
