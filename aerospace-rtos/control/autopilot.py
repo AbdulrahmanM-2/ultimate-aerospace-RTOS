edf19ff1fc328cdec77e776f37abdd3ec12b4eb7
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-04-01 23:17:45 +0300

    Add files via upload

---
+++ aerospace-rtos/control/autopilot.py
@@ -0,0 +1,3 @@
+
+def compute_waypoint_command(state, waypoint):
+    return {"roll": 0.0, "pitch": 0.0, "throttle": 0.7}
---
+++ aerospace-rtos/control/tmr.py
@@ -0,0 +1,5 @@
+
+def vote(a, b, c):
+    if a == b or a == c: return a
+    if b == c: return b
+    return None
